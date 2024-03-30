d2460edf35b676a248ebe3e7aeedaf79e4f54e24
Jorge Sosa <sosapererajorge@gmail.com>
2024-03-30 12:50:30 -0600

    Add twilio settings

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,13 +1,26 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\User;
 
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    public function submit()
+    public function submit(Request $request)
     {
-        
+        //validate the phone number 
+        $request->validate([
+            'phone' => 'required|numeric|min:10'
+        ]);
+
+        //fin or create a user model
+        $user = User::firstOrCreate([
+            'phone' => $request->phone
+        ]);
+
+        if(!$user){
+            return response()->json(['message' => 'Could not process a user with that phone number'], 401);
+        };
     }
 }
