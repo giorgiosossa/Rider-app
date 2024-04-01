4c472ad6aeb0d5d2ab3a0cef32c27d7eabdb4acb
Jorge Sosa <sosapererajorge@gmail.com>
2024-03-31 20:56:54 -0600

    finishin the login controller ready to start to test

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\User;
-
+use App\Notifications\LoginNeedsVerification;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
@@ -22,5 +22,40 @@ class LoginController extends Controller
         if(!$user){
             return response()->json(['message' => 'Could not process a user with that phone number'], 401);
         };
+
+        //send the user a one-time use code
+        $user->notify(new LoginNeedsVerification());
+
+        //return back a response
+        return response()->json(['message' => 'Text massage notification sent. ']);
+    }
+
+    public function verify(Request $request)
+    {
+        //validate the incoming request 
+        $request->validate([
+            'phone' => 'required|numeric|min:10',
+            'login_code' => 'required|numeric|between:111111,99999'
+        ]);
+
+
+        //find the user 
+        $user = User::where('phone', $request->phone)
+            ->where('login_code', $request->login_code)
+            ->first();
+
+
+        //is the code provided the same one saved?
+        //if so, return back the auth token 
+        if ($user){
+            $user->update([
+                'login_code' => null
+            ]);
+            return $user->createToken($request->login_code)->plaintextToken;
+        }
+
+
+        //if not, return back a message
+        return response()->json(['message' => 'Invalid verification code. '], 401);
     }
 }
--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -11,5 +11,13 @@ class Driver extends Model
 
     protected $guarded = [];
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
+    public function trips()
+    {
+        return $this->hasMany(Trip::class);
+    }
 }
